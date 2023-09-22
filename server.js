6df6e1e943ff94f6ca299bb5a646fad3955dc956
sucks555 <102584033+sucks-555@users.noreply.github.com>
2023-09-22 18:01:32 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,21 +4,28 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 const configPath = path.join(__dirname, 'config.json');
 const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
 const env = process.env;
 const correctPassword = env.password || 'NoPassword';
 const port = env.PORT || 3000;
-const exclusion = env.EXCLUSION || '';
-let IPAddress;
-let dir;
-app.use(bodyParser.json());
+const exclusion = env.EXCLUSION || '@';
 const videoExtensions = ['.mp4', '.mov', '.MP4', '.MOV'];
 const imageExtensions = ['.png', '.jpg', '.jpeg', '.webp', '.gif', '.PNG', '.JPG', '.JPEG', '.WEBP', '.GIF'];
-if (config.Access.local) { IPAddress = '127.0.0.1';
-} else { IPAddress = env.WiFi_IPAddress || '127.0.0.1'; }
-if (config.dirConditions.samedirectory) { dir = path.join(__dirname, '..', env.FOLDER);
-} else { dir = path.join(env.FOLDER || __dirname); }
+let IPAddress;
+let dir;
+
+if (config.Access.local) {
+  IPAddress = '127.0.0.1';
+} else {
+  IPAddress = env.WiFi_IPAddress || '127.0.0.1';
+}
+if (config.dirConditions.samedirectory) {
+  dir = path.join(__dirname, '..', env.FOLDER);
+} else {
+  dir = path.join(env.FOLDER || __dirname);
+}
 
 console.log('Directory:', dir);
 console.log('IPAddress:', IPAddress);
@@ -39,8 +46,10 @@ function getFiles(folderPath, extensionFilter, resultArray, genre = '') {
     }
   });
 }
-function log(Type, list) { list.forEach(item => console.log(`[${Type}] ${item}`)); }
 
+function log(Type, list) {
+  list.forEach(item => console.log(`[${Type}] ${item}`));
+}
 
 app.get(`/${env.IMAGE}`, (req, res) => {
   const listImage = [];
@@ -48,12 +57,14 @@ app.get(`/${env.IMAGE}`, (req, res) => {
   res.json(listImage);
   log('image', listImage);
 });
+
 app.get(`/${env.VIDEO}`, (req, res) => {
   const listVideo = [];
   getFiles(path.join(dir, env.VIDEO), videoExtensions, listVideo);
   res.json(listVideo);
   log('video', listVideo);
 });
+
 app.post('/password', (req, res) => {
   const { password } = req.body;
   if (password === correctPassword) {
@@ -62,6 +73,7 @@ app.post('/password', (req, res) => {
     res.status(401).send('Unauthorized');
   }
 });
+
 app.get('/path', (req, res) => {
   const pathJson = {
     'image': env.IMAGE || 'image',
@@ -76,9 +88,9 @@ app.get('/stop', () => {
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(`/${env.image}`, express.static(path.join(__dirname, env.image)));
+app.use(`/${env.IMAGE}`, express.static(path.join(__dirname, env.IMAGE)));
 app.use(`/${env.VIDEO}`, express.static(path.join(__dirname, env.VIDEO)));
 
 app.listen(port, IPAddress, () => {
-  console.log(`Server listening on port ${port}\nhttp://${IPAddress}:${port}/index.html`);
+  console.log(`Server listening on port ${port}\nhttp://${IPAddress}:${port}`);
 });
